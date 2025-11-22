db2298a5e9c81b94188d841cb9388642a4619cd3
juumelisa <jumelisah@gmail.com>
2025-11-22 13:12:00 +0700

    add middleware

---
+++ app/middleware/public.py
@@ -0,0 +1,14 @@
+from fastapi import Header, HTTPException
+import os
+
+def auth_public(x_api_key: str = Header(None)):
+  api_key = os.getenv("API_KEY")
+  if x_api_key != api_key:
+    raise HTTPException(
+      status_code=200,
+      detail={
+        "code" :401,
+        "message": "unauthorized",
+        "result": []
+      }
+    )
--- app/src/auth/router.py
+++ app/src/auth/router.py
@@ -1,12 +1,13 @@
 from fastapi import APIRouter, Depends, Header
 from sqlalchemy.orm import Session
 from app.config.database import get_db
+from app.middleware.public import auth_public
 from .schema import AuthData, AuthResponse
 from .controller import auth, remove_token
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
-@router.post("/", response_model=AuthResponse)
+@router.post("/", dependencies=[Depends(auth_public)])
 def check_auth(data: AuthData, db: Session = Depends(get_db)):
   return auth(data, db)
 
